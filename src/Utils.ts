f55da94f89f5e0f9d8352bb51875e25956806691
igorskyflyer <igor.dvlpr@gmail.com>
2019-01-05 00:06:51 +0100

    removed the URL validation for now

--- src/Utils.ts
+++ src/Utils.ts
@@ -9,17 +9,4 @@ export default abstract class Utils {
  
     return extension[extension.length - 1].toLowerCase();
   }
-
-  // validate the provided URL
-  public static isValidUrl(url: string): boolean {
-    if(url.length === 0) {
-      return false;
-    }
-
-    return true;
-
-    const pattern: RegExp = new RegExp(/^$/i);
-
-    return pattern.test(url);
-  }
 }
